f08095de155a45a7d96bf584fb4701f52e2e02c1
Jordan <jordan@ghost.st>
2024-10-29 00:12:14 +1300

    Add other collection defs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,13 @@
 import { defineCollection } from 'astro:content';
 
-const lore = defineCollection({ });
+const lore    = defineCollection({ });
+const entries = defineCollection({ });
+const meta    = defineCollection({ });
+const notes   = defineCollection({ });
 
 export const collections = {
-  'lore': lore,
+  lore,
+  entries,
+  meta,
+  notes,
 };
