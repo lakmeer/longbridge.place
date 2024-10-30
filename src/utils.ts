e4eb1ac45e244be7b279438910a79fc60d1d4d31
Jordan <jordan@ghost.st>
2024-10-30 19:52:43 +1300

    Add related entries to lore articles

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,18 @@
 
-import { getEntry } from 'astro:content';
+import { getEntry } from 'astro:content'
 
 export function absolutePath (path: string) {
-  return '/' + path.replace(/^\//, '');
+  return '/' + path.replace(/^\//, '')
 }
 
 export async function resolve (ref: string | { collection: string, slug: string }) {
   if (typeof ref === 'string') {
     return ref
   } else {
-    return await getEntry(ref.collection, ref.slug);
+    return await getEntry(ref.collection, ref.slug)
   }
+}
+
+export function entryUrl (entry: CollectionEntry) {
+  return `/${entry.collection}/${entry.slug}`
 }
